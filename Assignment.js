b6720ed015ab0b203dd238c1d401b665d156affc
Ayomide Saka <ayomidesaka@MacBook-Pro.local>
2024-08-16 13:11:41 +0100

    Project Management Dashboard

---
+++ Assignment.js
@@ -0,0 +1,102 @@
+//Assignment One
+/*let myName = `Babatunde Saka`
+let age = 24
+let isStudent = false
+
+console.log(`Name:`, myName)
+console.log(`Age:`, age)
+console.log(`Student:`, isStudent)*/
+
+//Assignment Two
+/*let person = {
+    firstName: `Babatunde`,
+    lastName: `Saka`,
+    age: 24,
+    isStudent: false,
+    fullName: function(){
+       let fullName = `${this.firstName} ${this.lastName}`;
+        return fullName
+    }
+    
+
+}
+
+console.log(person.fullName())/*
+
+//Assignment Three
+/*let squareRoot = Math.sqrt(64)
+console.log(squareRoot)
+
+let randomNumber = Math.random()
+console.log(randomNumber)
+
+let roundUp = Math.ceil(7.5)
+console.log(roundUp)
+
+let minimumNumber = Math.min(10, 20, 30, 40, 50)
+console.log(minimumNumber)*/
+
+
+//Assignment Four
+/*let a = 20
+let b = 2
+let addition = a+b
+let subtraction = a-b
+let multiplication = a*b
+let division = a/b
+let remainder = a%b
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+console.log(remainder)*/
+
+//Assignment Five
+
+/*let x = 10
+let y = x + 5
+x = 20
+
+console.log(x)
+console.log(y)*/
+
+//Assignment Six
+
+/*let book = {
+    title: `Rick Dad, Poor Dad`,
+    author: `Robert T Kyosaki`,
+    pages: 336,
+    isAvailable: true, 
+    summary: function(){
+
+        return `${this.title} by ${this.author} has ${this.pages} pages.
+        Availability: ${this.isAvailable}`
+    }
+
+}
+
+console.log(book.summary())*/
+
+//Assignment Seven
+/*let absoluteNumber = Math.abs(-45)
+let roundDown = Math.floor(7.8)
+let roundUp = Math.ceil(3.2)
+let raiseToPower = Math.pow(3,4)
+
+console.log(absoluteNumber)
+console.log(roundDown)
+console.log(roundUp)
+console.log(raiseToPower)*/
+
+//Assignment Eight
+/*let c = 30
+let d = 5
+let exponential = c ** d;
+c++
+d--
+
+console.log(exponential)
+console.log(c)
+console.log(d)*/
+
